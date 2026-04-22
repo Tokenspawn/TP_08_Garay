a997b350ad7f9211069a9837a486c7a6358dac89
Tokenspawn <pvz2joaco@gmail.com>
2026-04-22 08:52:17 -0300

    a

---
+++ Assets/Scritps/ValidarContraseña.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string ContraseñaCorrecta;
+    string ContraseñaUsuario;
+    public TextMeshProUGUI Placeholder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContraseñaCorrecta = "3314";    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ValidarContraseña()
+    {
+
+    }
+}

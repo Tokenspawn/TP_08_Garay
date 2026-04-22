a8cfa1abe0176f94656a129baf26e9f9ff9eb928
Tokenspawn <pvz2joaco@gmail.com>
2026-04-22 08:30:28 -0300

    Initial commit

---
+++ Assets/Scritps/PasswordUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PasswordUI : MonoBehaviour
+{
+    public TextMeshProUGUI txt_Resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckPassword();
+    }
+
+
+    public void CheckPassword()
+    {
+        txt_Resultado.text = "Acá va el resultado";
+    }
+}
